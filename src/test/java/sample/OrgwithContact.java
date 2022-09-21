8833de41622d1ea81d9b3090abfebe8fa69aa523
Kratika Rawat <kratikarawat20@gmail.com>
2022-09-21 12:22:40 +0100

    Branch TC_01 first commit

---
+++ src/test/java/sample/OrgwithContact.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class OrgwithContact {
+	
+	
+	@Test 
+	public void orgwcon() {
+		
+		
+		System.out.println("New testcase by enginner -1");
+	}
+
+}
